d4ed551d8710554a4aab226e00b56c148de93b5f
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-07-31 15:36:16 -0300

    :sparkles: feat: get all user boards

--- backend/src/api/board/board.controller.ts
+++ backend/src/api/board/board.controller.ts
@@ -1,12 +1,14 @@
+import { BoardSummaryDto } from './dto';
 import { BoardService } from './board.service';
 import { Controller, Get } from '@nestjs/common';
-import { BoardDto, BoardSummaryDto } from './dto';
 import { UserId } from '../../common/decorators/get-user-id.decorator';
 
 @Controller('board')
 export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
   @Get('/')
   async getUserBoards(@UserId() userId: number): Promise<BoardSummaryDto[]> {
-    return null;
+    return this.boardService.getUserBoards(userId);
   }
 }
--- backend/src/api/board/board.service.ts
+++ backend/src/api/board/board.service.ts
@@ -1,4 +1,32 @@
+import { BoardSummaryDto } from './dto';
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService,
+  ) {}
+
+  async getUserBoards(userId: number): Promise<BoardSummaryDto[]> {
+    const boards = await this.prisma.board.findMany({
+      where: { ownerId: userId },
+      include: {
+        columns: {
+          include: { cards: true },
+        },
+        owner: true,
+      },
+    });
+
+    if (!boards) return [];
+
+    const summaryBoard: BoardSummaryDto[] = boards.map((board) => {
+      return new BoardSummaryDto(board);
+    });
+
+    return summaryBoard;
+  }
+}
