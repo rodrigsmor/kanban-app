2f91936aee8104648d7c9c0d3201e6ffeb0110b4
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-07-30 22:31:15 -0300

    :construction: sets up the board module

--- backend/src/api/board/board.module.ts
+++ backend/src/api/board/board.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
 import { BoardService } from './board.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { BoardController } from './board.controller';
+import { AuthService } from '../../auth/auth.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { PrismaService } from '../../prisma/prisma.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AuthMiddleware } from '../../utils/middlewares/auth.middleware';
+import { MulterConfigService } from '../../utils/config/multer-config-service';
 
 @Module({
+  imports: [
+    JwtModule.register({}),
+    MulterModule.registerAsync({
+      useClass: MulterConfigService,
+    }),
+  ],
   controllers: [BoardController],
-  providers: [BoardService],
+  providers: [BoardService, AuthService, JwtService, PrismaService],
 })
-export class BoardModule {}
+export class BoardModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(BoardController);
+  }
+}
