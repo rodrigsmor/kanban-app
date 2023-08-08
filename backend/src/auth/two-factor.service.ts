caf1847b88ec02da1bc22c1ce6f2861e66495004
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-07 23:54:47 -0300

    :sparkles: feat: validate two factor tokens

--- backend/src/auth/two-factor.service.ts
+++ backend/src/auth/two-factor.service.ts
@@ -1,7 +1,12 @@
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Injectable } from '@nestjs/common';
 import * as randomstring from 'randomstring';
 import { PrismaService } from '../prisma/prisma.service';
+import { JwtPayload } from '../utils/@types/jwt.payload';
 
 type TwoFactorGenerationResponse = {
   verificationCode: string;
@@ -10,18 +15,20 @@ type TwoFactorGenerationResponse = {
 
 @Injectable()
 export class TwoFactorService {
+  private readonly jwtSecret: string;
+
   constructor(
     private readonly jwtService: JwtService,
     private readonly prisma: PrismaService,
-  ) {}
+  ) {
+    this.jwtSecret = process.env.JWT_SECRET_KEY;
+  }
 
   async generateTwoFactorToken(
     userId: number,
     email: string,
     expireAt: Date,
   ): Promise<TwoFactorGenerationResponse> {
-    const jwtSecret = process.env.JWT_SECRET_KEY;
-
     const verificationCode = randomstring.generate({
       length: 8,
       charset: 'numeric',
@@ -29,7 +36,7 @@ export class TwoFactorService {
 
     const token = await this.jwtService.signAsync(
       { email, sub: userId },
-      { secret: `${jwtSecret}-2Fa`, expiresIn: 30 * 60 },
+      { secret: `${this.jwtSecret}-2Fa`, expiresIn: 30 * 60 },
     );
 
     await this.prisma.twoFactor.create({
@@ -42,5 +49,42 @@ export class TwoFactorService {
     });
 
     return { token, verificationCode };
+  }
+
+  async validateTwoFactorTokens(
+    userId: number,
+    token: string,
+    code: string,
+  ): Promise<JwtPayload> {
+    try {
+      const payload: JwtPayload = await this.jwtService.verifyAsync(token, {
+        secret: `${this.jwtSecret}-2Fa`,
+      });
+
+      if (userId !== payload.sub)
+        throw new ForbiddenException(
+          'You do not have permission to perform this action',
+        );
+
+      const twoFactor = await this.prisma.twoFactor.findUnique({
+        where: { token },
+      });
+
+      if (twoFactor.verificationCode !== code)
+        throw new UnauthorizedException('the code provided is incorrect.');
+
+      if (!twoFactor)
+        throw new UnauthorizedException('it is not possible to authenticate');
+
+      await this.prisma.twoFactor.delete({
+        where: { id: twoFactor.id },
+      });
+
+      return payload;
+    } catch (error) {
+      throw new UnauthorizedException(
+        error?.message || 'The provided token is invalid.',
+      );
+    }
   }
 }
