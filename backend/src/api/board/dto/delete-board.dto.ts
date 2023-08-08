3ea6f357ac06254ea0cdbb9c12d06d67d9788085
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-08 00:04:51 -0300

    :label: feat: dtos to board deleting

---
+++ backend/src/api/board/dto/delete-board.dto.ts
@@ -0,0 +1,4 @@
+export class DeleteBoardDTO {
+  verificationCode: string;
+  token: string;
+}
--- backend/src/api/board/dto/index.ts
+++ backend/src/api/board/dto/index.ts
@@ -1,4 +1,5 @@
 export * from './board.dto';
 export * from './board-create.dto';
 export * from './board-invite.dto';
+export * from './delete-board.dto';
 export * from './board-summary.dto';
