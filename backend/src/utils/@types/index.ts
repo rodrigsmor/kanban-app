fe51d35c11d5a658c35d11c89501c8c71d1c73e5
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-11 22:08:10 -0300

    :label: feat: updates types exportions

--- backend/src/utils/@types/index.ts
+++ backend/src/utils/@types/index.ts
@@ -1,5 +1,6 @@
 export * from './card.types';
 export * from './jwt.payload';
 export * from './board.types';
+export * from './column.type';
 export * from './payloads.type';
 export * from './invite-data.types';
