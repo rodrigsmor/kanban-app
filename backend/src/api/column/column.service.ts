6fccf747a0678a8286343442f40aae35b0673328
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-14 22:33:26 -0300

    :sparkles: feat: update column service

--- backend/src/api/column/column.service.ts
+++ backend/src/api/column/column.service.ts
@@ -3,9 +3,9 @@ import {
   InternalServerErrorException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { CreateColumnDto } from './dto';
 import { ColumnType } from '../../utils/@types';
 import { UserService } from '../user/user.service';
+import { CreateColumnDto, EditColumnDto } from './dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import { BoardRepository } from '../../common/repositories/board.repository';
 
@@ -54,5 +54,54 @@ export class ColumnService {
     );
 
     return columns;
+  }
+
+  async updateColumn(
+    userId: number,
+    boardId: number,
+    columnData: EditColumnDto,
+  ): Promise<ColumnType[]> {
+    const hasPermission =
+      await this.boardRepository.checkIfMemberHasPermissionToEdit(
+        userId,
+        boardId,
+      );
+
+    if (!hasPermission)
+      throw new UnauthorizedException(
+        'you do not have permission to perform this action',
+      );
+
+    try {
+      await this.prisma.column.update({
+        where: { id: columnData.columnId },
+        data: {
+          ...(columnData?.title && { title: columnData?.title }),
+          ...(columnData?.columnIndex && {
+            columnIndex: columnData?.columnIndex,
+          }),
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'it was not possible to update your column',
+      );
+    }
+
+    const board = await this.boardRepository.findBoardById(boardId, userId);
+
+    const columns: ColumnType[] = board.columns.map(
+      (column) => new ColumnType(column),
+    );
+
+    return columns;
+  }
+
+  async deleteColumn(
+    userId: number,
+    boardId: number,
+    columnId: number,
+  ): Promise<ColumnType[]> {
+    return [];
   }
 }
