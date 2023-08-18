39c372c638cc3620aa119bcf3518524c1e3f1172
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-17 22:46:14 -0300

    :label: feat: adds documentation to api properties

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
+  @ApiProperty({
+    required: true,
+    example: 'ana.julia@example.com',
+    description: 'the email address of the user.',
+  })
   @IsEmail(undefined, {
     message: 'the e-mail entered is not a valid entry.',
   })
@@ -10,6 +16,10 @@ export class AuthDto {
   @IsString({ message: 'the e-mail should be a string entry.' })
   email: string;
 
+  @ApiProperty({
+    description: 'the password of the user account',
+    required: true,
+  })
   @IsNotEmpty({ message: 'the password is required to log in' })
   @IsString({ message: 'the password should be a string entry' })
   password: string;
--- backend/src/auth/dto/signup.dto.ts
+++ backend/src/auth/dto/signup.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignupDto {
+  @ApiProperty({
+    example: 'Ana Júlia',
+    required: true,
+    description: 'the first name of the user',
+  })
   @IsString({ message: 'your first name should be a string entry.' })
   @IsNotEmpty({ message: 'you must enter your name in order to register.' })
   firstName: string;
 
+  @ApiProperty({
+    example: 'Menezes Machado',
+    required: true,
+    description: 'the last name of the user',
+  })
   @IsString({ message: 'your last name should be a string entry.' })
   @IsNotEmpty({ message: 'your last name is required to register.' })
   lastName: string;
 
+  @ApiProperty({
+    required: true,
+    example: 'ana.julia@example.com',
+    description: 'the e-mail address of the user',
+  })
   @IsEmail(undefined, {
     message: 'the e-mail entered is not a valid entry.',
   })
@@ -18,6 +34,10 @@ export class SignupDto {
   @IsString({ message: 'the e-mail should be a string entry.' })
   email: string;
 
+  @ApiProperty({
+    required: true,
+    description: 'the password of the user account',
+  })
   @IsString({ message: 'your password should be a string entry.' })
   @IsNotEmpty({ message: 'it is not possible to use an empty password.' })
   password: string;
