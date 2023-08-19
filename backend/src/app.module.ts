745e4b06ef04f46ca5fe4bee80ea9ff302bde783
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-19 19:18:24 -0300

    :ok_hand: style: adds Card module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
+import { CardModule } from './api/card/card.module';
 import { UserModule } from './api/user/user.module';
 import { BoardModule } from './api/board/board.module';
 import { PrismaService } from './prisma/prisma.service';
@@ -13,6 +14,7 @@ import { ColumnModule } from './api/column/column.module';
     UserModule,
     BoardModule,
     ColumnModule,
+    CardModule,
   ],
   controllers: [],
   providers: [PrismaService],
