4c69316ac27eaa0151f052ef9d55804f0416f808
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-19 19:30:43 -0300

    :sparkles: feat: creates Card module

---
+++ backend/src/api/card/card.module.ts
@@ -0,0 +1,35 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CardService } from './card.service';
+import { CardController } from './card.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { MulterModule } from '@nestjs/platform-express';
+import { MulterConfigService } from 'src/utils/config';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from '../user/user.service';
+import { BoardRepository } from '../../common/repositories/board.repository';
+import { AuthService } from '../../auth/auth.service';
+import { AuthMiddleware } from '../../utils/middlewares/auth.middleware';
+import { CardRepository } from '../../common/repositories/card.repository';
+
+@Module({
+  imports: [
+    JwtModule.register({}),
+    MulterModule.registerAsync({
+      useClass: MulterConfigService,
+    }),
+  ],
+  controllers: [CardController],
+  providers: [
+    CardService,
+    PrismaService,
+    UserService,
+    AuthService,
+    BoardRepository,
+    CardRepository,
+  ],
+})
+export class CardModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(CardController);
+  }
+}
