e86f8e50cb1013bbbe9b0719e4c46de7ff2e92e9
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-18 22:42:58 -0300

    :ok_hand: style: remove deleted imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
 import { UserModule } from './api/user/user.module';
 import { BoardModule } from './api/board/board.module';
 import { PrismaService } from './prisma/prisma.service';
@@ -16,7 +14,7 @@ import { ColumnModule } from './api/column/column.module';
     BoardModule,
     ColumnModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
