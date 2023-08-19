955a780153bf170a539025bb6a3eda29e230d907
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-18 21:34:25 -0300

    :label: feat: documents column type

--- backend/src/utils/@types/column.type.ts
+++ backend/src/utils/@types/column.type.ts
@@ -1,13 +1,44 @@
 import { Card } from '@prisma/client';
 import { ColumnPrismaType } from './';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ColumnType {
+  @ApiProperty({
+    description: 'the id of the column',
+  })
   id: number;
+
+  @ApiProperty({
+    example: '✅ To-do',
+    description: 'the title of this column',
+  })
   title: string;
+
+  @ApiProperty({
+    example: '/images/column-cover-example.png',
+    description: 'the path to the card cover',
+  })
   cover: string;
+
+  @ApiProperty({
+    description: 'the date this board was created.',
+  })
   createdAt: Date;
+
+  @ApiProperty({
+    description: 'the date when the last update was made to this board.',
+  })
   updateAt: Date;
+
+  @ApiProperty({
+    description: '',
+  })
   cards: Array<Card>;
+
+  @ApiProperty({
+    example: 1,
+    description: 'the column index of the column',
+  })
   columnIndex: number;
 
   constructor(column: ColumnPrismaType) {
