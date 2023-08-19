0e1a4927ea4fcca29afd4c3642434d87c5b5d9c2
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-18 21:31:16 -0300

    :sparkles: feat: adds documentation to board controller

--- backend/src/api/board/board.controller.ts
+++ backend/src/api/board/board.controller.ts
@@ -19,14 +19,32 @@ import {
 import { UserDto } from '../user/dto';
 import { BoardService } from './board.service';
 import { BoardRolesEnum } from '../../utils/enums';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserId } from '../../common/decorators/get-user-id.decorator';
 
+@ApiTags('Board')
 @Controller('/api/board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
   @Get('/')
   @HttpCode(HttpStatus.ACCEPTED)
+  @ApiOperation({
+    description:
+      'This endpoint will get all the boards the user is a member of.',
+  })
+  @ApiResponse({
+    status: 202,
+    type: BoardSummaryDto,
+    isArray: true,
+    description:
+      'It will return a summary list of the boards the user is a member of.',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+  })
   async getUserBoards(
     @UserId() userId: number,
     @Query('limit') limit?: number,
@@ -36,6 +54,20 @@ export class BoardController {
 
   @Get('/owned')
   @HttpCode(HttpStatus.ACCEPTED)
+  @ApiOperation({
+    description: 'This endpoint will get all the boards owned by the user.',
+  })
+  @ApiResponse({
+    status: 202,
+    type: BoardSummaryDto,
+    isArray: true,
+    description: 'It will return a summary list of boards owned by the user.',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+  })
   async getOwnedBoards(
     @UserId() userId: number,
     @Query('limit') limit?: number,
@@ -45,6 +77,15 @@ export class BoardController {
 
   @Get('/:id')
   @HttpCode(HttpStatus.ACCEPTED)
+  @ApiOperation({
+    description:
+      'This endpoint will get board information according to the id entered in the request.',
+  })
+  @ApiResponse({
+    status: 202,
+    type: BoardDto,
+    description: 'It will return the board information.',
+  })
   async getBoard(
     @UserId() userId: number,
     @Param('id') boardId: number,
@@ -54,6 +95,14 @@ export class BoardController {
 
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    description: 'This endpoint will create a new board.',
+  })
+  @ApiResponse({
+    status: 202,
+    type: BoardSummaryDto,
+    description: 'It will return the summarized board that was just created.',
+  })
   async createNewBoard(
     @UserId() userId: number,
     @Body() newBoard: BoardCreateDto,
@@ -63,6 +112,14 @@ export class BoardController {
 
   @Patch('/:boardId/')
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'This endpoint will change the role of a board member.',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UserDto,
+    description: 'It will return the new member data.',
+  })
   async updateMemberRole(
     @Param('boardId') boardId: number,
     @Query('memberId') memberId: number,
@@ -74,6 +131,15 @@ export class BoardController {
 
   @Post('/delete/:boardId/init')
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    description:
+      'This endpoint will start a new board deletion process. To delete a board, you must confirm that you are a member who has permission to perform this action, as well as confirm that this procedure is actually being requested by the member.',
+  })
+  @ApiResponse({
+    status: 201,
+    type: String,
+    description: 'It will start the process and return a authentication token.',
+  })
   async initiateBoardDeletion(
     @UserId() userId: number,
     @Param('boardId') boardId: number,
@@ -83,6 +149,10 @@ export class BoardController {
 
   @Delete('/:boardId')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    description:
+      'This endpoint will verify that the data entered is correct, in this case the verification code sent by email and also the authentication token. If it is correct, it will proceed with the process, thus deleting the board.',
+  })
   async deleteBoard(
     @UserId() userId: number,
     @Param('boardId') boardId: number,
