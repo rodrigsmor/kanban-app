e0fd1a9a5421abb490d124cd3454358a37150f5d
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-18 21:32:52 -0300

    :label: feat: documents board classes

--- backend/src/api/board/dto/board-create.dto.ts
+++ backend/src/api/board/dto/board-create.dto.ts
@@ -1,11 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class BoardCreateDto {
+  @ApiProperty({
+    description: 'the name of the board',
+    example: 'My daily tasks',
+  })
   @IsString({
     message: 'the name must be a string value',
   })
   name: string;
 
+  @ApiProperty({
+    description: 'A description of the board.',
+    example:
+      'A board where I organize my daily tasks and also plan myself to meet my main goals.',
+  })
   @IsString({
     message: 'the description must be a string value',
   })
--- backend/src/api/board/dto/board-summary.dto.ts
+++ backend/src/api/board/dto/board-summary.dto.ts
@@ -1,16 +1,57 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { UserDto } from '../../user/dto/user.dto';
 import { BoardPrismaType, BoardWithColumns } from '../../../utils/@types';
 import { getAmountOfCards } from '../../../utils/functions/get-amount-of-cards';
 
 export class BoardSummaryDto {
+  @ApiProperty({
+    description: 'the id of the board',
+  })
   id: number;
+
+  @ApiProperty({
+    description: 'the name of the board',
+    example: 'My daily tasks',
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'the date when the last update was made to this board.',
+  })
   updateAt: Date;
+
+  @ApiProperty({
+    description: 'the user who owns this board.',
+  })
   owner: UserDto;
+
+  @ApiProperty({
+    description: 'the date this board was created.',
+  })
   createdAt: Date;
+
+  @ApiProperty({
+    description: 'if the current member pinned this board.',
+  })
   isPinned: boolean;
+
+  @ApiProperty({
+    description: 'A description of the board.',
+    example:
+      'A board where I organize my daily tasks and also plan myself to meet my main goals.',
+  })
   description: string;
+
+  @ApiProperty({
+    description: 'the number of cards and/or tasks on that board',
+    example: 12,
+  })
   totalCards: number;
+
+  @ApiProperty({
+    example: 4,
+    description: 'The number of members on this board.',
+  })
   numberOfMembers: number;
 
   constructor(board: BoardWithColumns | BoardPrismaType) {
