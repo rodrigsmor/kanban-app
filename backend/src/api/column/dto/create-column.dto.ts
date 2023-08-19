3270467d6f08fc7ce93b523a82ab160a0be28277
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-18 22:38:41 -0300

    :label: feat: documents column types

--- backend/src/api/column/dto/create-column.dto.ts
+++ backend/src/api/column/dto/create-column.dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateColumnDto {
+  @ApiProperty({
+    example: '✅ To-do',
+    description: 'the title of this column',
+  })
   @IsString({
     message: 'the title must be a string',
   })
   title?: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'the column index of the column',
+  })
   @IsNumber(undefined, {
     message: 'the column index should be a number',
   })
