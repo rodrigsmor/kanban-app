ce493a9f7c8bf6fbd651206e6be755acbcc2bbe4
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-08-19 23:20:09 -0300

    :goal_net: feat: adds assigneesIds and validate

--- backend/src/api/card/dto/create-card.dto.ts
+++ backend/src/api/card/dto/create-card.dto.ts
@@ -1,3 +1,10 @@
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCardDto {
@@ -5,12 +12,25 @@ export class CreateCardDto {
     required: true,
     example: '🔜 Write the project README',
   })
+  @IsString({
+    message: 'the title needs to be a string',
+  })
+  @IsNotEmpty({
+    message: 'the card needs a title to be created',
+  })
   title: string;
 
   @ApiProperty({
     example: 0,
     required: true,
     description: 'the id of the column the card is part of',
+  })
+  @IsNotEmpty({
+    message:
+      'it is necessary to indicate which column the card is being created',
+  })
+  @IsNumber(undefined, {
+    message: 'the column id needs to be a number',
   })
   columnId: number;
 
@@ -22,4 +42,20 @@ export class CreateCardDto {
       'Create an informative and concise project README that outlines key features, installation steps, and usage instructions. Give users a clear roadmap to understand and engage with your project effectively.',
   })
   description?: string;
+
+  @ApiProperty({
+    isArray: true,
+    required: false,
+    description:
+      'This field is an array of members who serves as the assignees for this card',
+  })
+  @IsOptional()
+  @IsArray({
+    message: 'assigneesIds should be an array of numbers',
+  })
+  @IsNumber(
+    {},
+    { each: true, message: 'Each item in assigneesIds should be a number' },
+  )
+  assigneesIds?: number[];
 }
