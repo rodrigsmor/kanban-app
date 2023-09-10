af2956396abf3339c92057e43336a6e1cf6ef5ef
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2023-09-09 23:38:35 -0300

    :construction: feat: adds i18module to app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,26 @@ import { BoardModule } from './api/board/board.module';
 import { LabelModule } from './api/label/label.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ColumnModule } from './api/column/column.module';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '../src/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
+      typesOutputPath: path.join(
+        __dirname,
+        '../src/utils/@types/i18n.translations.d.ts',
+      ),
+    }),
     AuthModule,
     JwtModule.register({}),
     UserModule,
